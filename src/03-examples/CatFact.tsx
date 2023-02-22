d691d80d73cb65eb4f4d7f6dde8a08bba0f4492d
Javier Tapia <javier.tapia.d@gmail.com>
2023-02-22 17:30:21 -0300

    [ADD] 5 - useLayoutEffect

--- src/03-examples/CatFact.tsx
+++ src/03-examples/CatFact.tsx
@@ -1,9 +1,21 @@
+import { useLayoutEffect, useRef, useState } from "react"
+
 type CatFactProps = {
     fact: string
 }
 
 export function CatFact({fact}: CatFactProps) {
+    const quoteRef = useRef<HTMLParagraphElement>(null)
+
+    const [boxHeight, setBoxHeight] = useState(0)
+
+    // Como useEffect, pero se ejecuta después de que se dibuja el DOM
+    useLayoutEffect(() => {
+        // const {height} = quoteRef.current?.getBoundingClientRect() || 0
+        setBoxHeight(quoteRef.current?.getBoundingClientRect().height || 0)
+    })
+
     return (
-        <blockquote>{fact}</blockquote>
+        <blockquote><p ref={quoteRef}>{fact}</p>{boxHeight}</blockquote>
     )
 }
